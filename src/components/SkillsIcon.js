8e8802fc12123b4cded2d44247db867728a750eb
stetsonharding <stetson2001@msn.com>
2022-05-06 10:15:04 -0700

    skill icons.

--- src/components/SkillsIcon.js
+++ src/components/SkillsIcon.js
@@ -1,27 +1,15 @@
 import React from "react";
 
+import "../css/SkillsIcon.css";
+
 function SkillsIcon(props) {
   return (
-    <div className="Skill-icon">
+    <div className="Skill-icon-container">
       <div className="icon">
-        <img
-          style={{ width: "95px", height: "95px" }}
-          src={props.src}
-          alt={props.alt}
-        />
+        <img src={props.src} alt={props.alt} />
       </div>
-      <div className="icon-name" style={{ textAlign: "center" }}>
-        <pre
-          style={{
-            padding: "0",
-            margin: "5px",
-            fontWeight: "bold",
-            fontSize: "12px",
-            fontStyle: "italic",
-          }}
-        >
-          HTML
-        </pre>
+      <div className="icon-name-container">
+        <pre>{props.name}</pre>
       </div>
     </div>
   );
--- src/containers/SkillsContainer.js
+++ src/containers/SkillsContainer.js
@@ -3,7 +3,16 @@ import SkillsIcon from "../components/SkillsIcon";
 
 import "../css/SkillsContainer.css";
 
-import htmlIcon from "../assets/testicon.PNG";
+import htmlIcon from "../assets/icons/html.webp";
+import cssIcon from "../assets/icons/css.webp";
+import javsScriptIcon from "../assets/icons/javaScript.webp";
+import reactIcon from "../assets/icons/react.webp";
+import reduxIcon from "../assets/icons/redux.webp";
+import sassIcon from "../assets/icons/sass.webp";
+import bootstrapIcon from "../assets/icons/bootstrap.webp";
+import graphQLIcon from "../assets/icons/graphQL.webp";
+import firebaseIcon from "../assets/icons/firebase.webp";
+import gitIcon from "../assets/icons/git.webp";
 
 function SkillsContainer() {
   return (
@@ -16,42 +25,58 @@ function SkillsContainer() {
           <tr>
             <div className="table-row">
               <th>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={htmlIcon} alt="html-icon" name="html" />
               </th>
               <th>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={cssIcon} alt="css-icon" name="css" />
               </th>
               <th>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={sassIcon} alt="sass-icon" name="sass" />
               </th>
             </div>
           </tr>
           <tr>
             <div className="table-row-mid">
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon
+                  src={javsScriptIcon}
+                  alt="JavaScript-icon"
+                  name="javaScript"
+                />
               </td>
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={reactIcon} alt="react-icon" name="react" />
               </td>
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={reduxIcon} alt="redux-icon" name="redux" />
               </td>
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon
+                  src={bootstrapIcon}
+                  alt="bootstrap-icon"
+                  name="bootstrap"
+                />
               </td>
             </div>
           </tr>
           <tr>
             <div className="table-row">
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon
+                  src={graphQLIcon}
+                  alt="graphQL-icon"
+                  name="graphql"
+                />
               </td>
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon
+                  src={firebaseIcon}
+                  alt="firebase-icon"
+                  name="firebase"
+                />
               </td>
               <td>
-                <SkillsIcon src={htmlIcon} alt="html-icon" />
+                <SkillsIcon src={gitIcon} alt="git-icon" name="git" />
               </td>
             </div>
           </tr>
