352e16e2bda40e20270b4a3e831ec352125a2619
stetsonharding <stetson2001@msn.com>
2022-05-11 06:54:16 -0700

    Added project images for RecentProject Component.

--- src/components/RecentProject.js
+++ src/components/RecentProject.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "../css/RecentProject.css";
+
+import ImageShop from "../assets/recentProjects/imageShop.webp";
 
 function RecentProject() {
-  return <div>RecentProject</div>;
+  const [displayImgInfo, setDisplayImgInfo] = useState(true);
+  return (
+    <div className="recent-project-container">
+      <div className="recent-project-image">
+        <img
+          className="project-image"
+          src={ImageShop}
+          alt="Mock up"
+          style={{ height: "250px", width: "350px" }}
+        />
+      </div>
+      <div classname="recent-project-tech-stack"></div>
+    </div>
+  );
 }
 
 export default RecentProject;
--- src/containers/RecentProjectsContainer.js
+++ src/containers/RecentProjectsContainer.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "../css/RecentProjectsContainer.css";
 
+import RecentProject from "../components/RecentProject";
+
 function RecentProjectsContainer() {
   return (
     <div className="recent-projects-container">
@@ -10,7 +12,9 @@ function RecentProjectsContainer() {
         <h1 className="heading">Recent Projects</h1>
       </div>
 
-      <div className="recent-projects"></div>
+      <div className="recent-projects">
+        <RecentProject />
+      </div>
     </div>
   );
 }
