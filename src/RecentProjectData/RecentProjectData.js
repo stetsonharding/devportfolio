f0aa3355b5723df7ce26c985f1c3f11a20cb771e
stetsonharding <stetson2001@msn.com>
2022-07-14 07:17:58 -0700

    added musical sweatpants project.

--- src/RecentProjectData/RecentProjectData.js
+++ src/RecentProjectData/RecentProjectData.js
@@ -1,47 +1,39 @@
 import ImageShop from "../assets/recentProjects/imageShop.webp";
 import ufoSightings from "../assets/recentProjects/ufoSightings.webp";
 import ValleyLawnPros from "../assets/recentProjects/valleyLawnPros.webp";
-// import noteDrop from "../assets/recentProjects/noteDrop.webp";
-import pokidex from "../assets/recentProjects/pokidex.webp";
+import MusicalSweatpants from "../assets/recentProjects/MusicalSweatpants.webp";
 
 export const RecentProjectData = [
   {
     id: 1,
     Imgsrc: ufoSightings,
-    Imgalt: "UFO Sightings Website Layout.",
+    Imgalt: "UFO Sightings Project.",
     description:
       "Get information on real UFO sightings and their coordinates to view on Google Maps. These are reported sightings around the USA. Made with React.js and Firebase for backend.",
     href: "https://stetsonharding.github.io/ufoSightings/#/",
   },
   {
     id: 2,
+    Imgsrc: MusicalSweatpants,
+    Imgalt: "Musical Sweatpants Project.",
+    description:
+      "Application that allows the user to shop for an album cover to print on sweatpants. Search, Add to cart, remove, and checkout!",
+    href: "https://stetsonharding.github.io/musical-sweatpants/",
+  },
+  {
+    id: 3,
     Imgsrc: ImageShop,
-    Imgalt: "Image Shop Website Layout.",
+    Imgalt: "Image Shop Project.",
     description:
       "Fetched unsplash API to shop for images. Allowing user to search, add/remove and favorite images for later use. Completed with a mocked checkout workflow.",
     href: "https://stetsonharding.github.io/imageShopApp/#/",
   },
   {
-    id: 3,
+    id: 4,
     Imgsrc: ValleyLawnPros,
-    Imgalt: "Valley Lawn Pros Website Layout.",
+    Imgalt: "Valley Lawn Pros Website.",
     description:
       "Website I built for a local landscaping company with React.js and bootstrap, hosted with Microsoft Azure.",
     href: "https://valleylawnpros.net/",
-  },
-  // {
-  //   id: 4,
-  //   Imgsrc: noteDrop,
-  //   Imgalt: "Note Drop Website Layout.",
-  //   description: "CRUD application for taking/removing notes.",
-  //   href: "https://stetsonharding.github.io/todo-drop/",
-  // },
-  {
-    id: 4,
-    Imgsrc: pokidex,
-    Imgalt: "Pokidex website Layout.",
-    description:
-      "Get details, Look up, search, and filter your favorite pokemons. Used GraphQL for API endpoint.",
-    href: "https://stetsonharding.github.io/pokedex-graphql/",
   },
 ];
