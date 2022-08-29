27330108009a8ae1fe0321ba55cefc7278dd78ff
stetsonharding <stetson2001@msn.com>
2022-08-29 06:16:39 -0700

    Pizza Order project added.

--- src/RecentProjectData/RecentProjectData.js
+++ src/RecentProjectData/RecentProjectData.js
@@ -1,7 +1,8 @@
-import ImageShop from "../assets/recentProjects/imageShop.webp";
+//import ImageShop from "../assets/recentProjects/imageShop.webp";
 import ufoSightings from "../assets/recentProjects/ufoSightings.webp";
 import ValleyLawnPros from "../assets/recentProjects/valleyLawnPros.webp";
 import MusicalSweatpants from "../assets/recentProjects/MusicalSweatpants.webp";
+import ReactPizzaOrders from "../assets/recentProjects/PizzaOrdersReact.webp";
 
 export const RecentProjectData = [
   {
@@ -14,19 +15,19 @@ export const RecentProjectData = [
   },
   {
     id: 2,
+    Imgsrc: ReactPizzaOrders,
+    Imgalt: "Blitz Pizza Orders.",
+    description:
+      "Blitz Pizza Co. An application that allows the user to find, add, edit, and delete their food orders within their cart. Build with React.js using Styled Complonents!",
+    href: "https://stetsonharding.github.io/react-pizza-orders/",
+  },
+  {
+    id: 3,
     Imgsrc: MusicalSweatpants,
     Imgalt: "Musical Sweatpants Project.",
     description:
       "Application that allows the user to shop for an album cover to print on sweatpants. Search, Select Size, Add to cart, remove, and checkout!",
     href: "https://stetsonharding.github.io/musical-sweatpants/",
-  },
-  {
-    id: 3,
-    Imgsrc: ImageShop,
-    Imgalt: "Image Shop Project.",
-    description:
-      "Fetched unsplash API to shop for images. Allowing user to search, add/remove and favorite images for later use. Completed with a mocked checkout workflow.",
-    href: "https://stetsonharding.github.io/imageShopApp/#/",
   },
   {
     id: 4,
